dbee0ed3c48cf2ea657371f124d67ec696867801
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-08-22 10:37:05 -0400

    Add plant validations

--- app/models/house_plant.rb
+++ app/models/house_plant.rb
@@ -1,6 +1,9 @@
 class HousePlant < ApplicationRecord
   belongs_to :room
 
+  validates :plant_type, :height, :room, presence: true
+  validates :height, numericality: { greater_than_or_equal_to: 1 }
+
   def room_name
     room.name
   end
