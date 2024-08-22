f4526eb4fe88ac95bdadb19ed2541f84655c85ff
Stephen McKeon <stephen.mckeon@powerhrg.com>
2024-08-22 14:26:14 -0400

    Add info to room show page

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,4 +2,9 @@ class RoomsController < ApplicationController
   def index
     @rooms = Room.all
   end
+
+  def show
+    @room = Room.find(params[:id])
+    @house_plants = @room.house_plants
+  end
 end
