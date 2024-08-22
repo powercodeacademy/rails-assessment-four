7bec6f3f3b4b053f54c728d2be96c8fdb2721a64
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-08-22 10:28:04 -0400

    Add create and new actions for plants

--- app/controllers/house_plants_controller.rb
+++ app/controllers/house_plants_controller.rb
@@ -6,4 +6,22 @@ class HousePlantsController < ApplicationController
   def show
     @house_plant = HousePlant.find(params[:id])
   end
+
+  def new
+    @house_plant = HousePlant.new
+    @rooms = Room.all
+  end
+
+  def create
+    house_plant = HousePlant.new(house_plant_params)
+    if house_plant.save
+      redirect_to house_plant_path(house_plant)
+    else
+      render :new
+    end
+  end
+
+  def house_plant_params
+    params.require(:house_plant).permit(:plant_type, :height, :room_id)
+  end
 end
