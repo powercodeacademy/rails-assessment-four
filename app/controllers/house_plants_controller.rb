472e812eeda88f7655ed790d40eeb7fa308762e6
Stephen McKeon <stephen.mckeon@powerhrg.com>
2024-08-22 14:22:17 -0400

    Add info to plant show page

--- app/controllers/house_plants_controller.rb
+++ app/controllers/house_plants_controller.rb
@@ -2,4 +2,9 @@ class HousePlantsController < ApplicationController
   def index
     @house_plants = HousePlant.all
   end
+
+  def show
+    @house_plant = HousePlant.find(params[:id])
+    @room = @house_plant.room
+  end
 end
