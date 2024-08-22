94e9355ac12225106fdcac0ad4ce3634758694e9
Stephen McKeon <stephen.mckeon@powerhrg.com>
2024-08-22 14:48:29 -0400

    Create new house plant

--- app/controllers/house_plants_controller.rb
+++ app/controllers/house_plants_controller.rb
@@ -7,4 +7,20 @@ class HousePlantsController < ApplicationController
     @house_plant = HousePlant.find(params[:id])
     @room = @house_plant.room
   end
+
+  def new
+    @house_plant = HousePlant.new
+  end
+
+  def create
+    house_plant = HousePlant.create!(house_plant_params)
+
+    redirect_to house_plant
+  end
+
+  private
+
+  def house_plant_params
+    params.require(:house_plant).permit(:plant_type, :height, :room_id)
+  end
 end
