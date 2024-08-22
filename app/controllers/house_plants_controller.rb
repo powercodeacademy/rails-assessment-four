dfffdde59128c9868cfec0b9d43118c60e225f03
Cayla Horsey <cayla.horsey@powerhrg.com>
2024-08-22 10:05:32 -0400

    Add form to new view and sync controller actions

--- app/controllers/house_plants_controller.rb
+++ app/controllers/house_plants_controller.rb
@@ -6,4 +6,27 @@ class HousePlantsController < ApplicationController
   def show
     @house_plant = HousePlant.find(params[:id])
   end
+
+  def new
+    @house_plant = HousePlant.new
+    @rooms = Room.all
+  end
+
+  def create
+    @house_plant = HousePlant.new(house_plant_params)
+    @room = @house_plant.room
+
+    if @house_plant.save
+      redirect_to room_path(@room)
+    else
+      flash.now.alert = @house_plant.errors.full_messages.to_sentence
+      render :new
+    end
+  end
+
+  private
+
+  def house_plant_params
+    params.require(:house_plant).permit(:room_id, :plant_type, :height)
+  end
 end
