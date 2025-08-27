7f2e8308d26e5245fba871564b47b5465407e422
Nihar Patel <niharpatel52@gmail.com>
2025-08-27 13:54:19 -0400

    Add link to HousePlan#inxed to point each [lant to HousePlant#show for each

--- app/controllers/house_plants_controller.rb
+++ app/controllers/house_plants_controller.rb
@@ -1,5 +1,20 @@
 class HousePlantsController < ApplicationController
+  before_action :set_house_plant, only: [:show]
+
   def index
     @house_plants = HousePlant.all
+  end
+
+  def show
+  end
+
+  private
+
+  def set_house_plant
+    @house_plant = HousePlant.find(params[:id])
+  end
+
+  def house_plant_params
+    params.require(:house_plant).permit(:plant_type, :height, :room_id)
   end
 end
