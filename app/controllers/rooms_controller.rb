883e9ef602a604352badffee0f76b31cac692f9c
Nihar Patel <niharpatel52@gmail.com>
2025-08-27 14:11:55 -0400

    Add details to HousePlants#show and room links to Room#show page

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,5 +1,19 @@
 class RoomsController < ApplicationController
+  before_action :set_room, only: [:show]
   def index
     @rooms = Room.all
+  end
+
+  def show
+  end
+
+  private
+
+  def set_room
+    @room = Room.find(params[:id])
+  end
+
+  def room_params
+    params.require(:room).permit(:name, :occupancy)
   end
 end
