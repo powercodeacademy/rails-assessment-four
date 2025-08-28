c452a391084b1531e626cafc438baf292adab1f4
Shantel <Shantelgray24@gmail.com>
2025-08-28 09:40:14 -0400

    Mock Challenge 3

--- app/controllers/house_plants_controller.rb
+++ app/controllers/house_plants_controller.rb
@@ -2,4 +2,27 @@ class HousePlantsController < ApplicationController
   def index
     @house_plants = HousePlant.all
   end
+
+  def show
+    @house_plant = HousePlant.find(params[:id])
+  end
+
+  def new
+    @house_plant = HousePlant.new
+    @rooms = Room.all
+  end
+  def create 
+    @house_plant = HousePlant.create(house_plant_params)
+    if @house_plant.save
+      redirect_to @house_plant
+    else
+      render :new
+    end
+  end
+
+  private
+  
+  def house_plant_params
+    params.require(:house_plant).permit(:plant_type, :height, :room_id)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,10 @@ ActiveRecord::Schema[7.1].define(version: 2019_06_10_081955) do
   create_table "house_plants", force: :cascade do |t|
     t.string "plant_type"
     t.integer "height"
+    t.integer "room_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["room_id"], name: "index_house_plants_on_room_id"
   end
 
   create_table "rooms", force: :cascade do |t|
@@ -21,4 +25,5 @@ ActiveRecord::Schema[7.1].define(version: 2019_06_10_081955) do
     t.integer "occupancy"
   end
 
+  add_foreign_key "house_plants", "rooms"
 end
