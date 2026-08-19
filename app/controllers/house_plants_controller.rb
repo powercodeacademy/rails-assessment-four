434cd93c8dc25f6007a5ab86d0150ab928fb3328
BarkleyRhoat <barkleyrhoat@gmail.com>
2026-08-19 11:17:40 -0400

    rails assessment four

--- app/controllers/house_plants_controller.rb
+++ app/controllers/house_plants_controller.rb
@@ -6,4 +6,23 @@ class HousePlantsController < ApplicationController
   def show
     @house_plant = HousePlant.find(params[:id])
   end
+
+  def new
+    @house_plant = HousePlant.new
+  end
+
+  def create
+    @house_plant = HousePlant.new(house_plant_params)
+    if @house_plant.save
+      redirect_to @house_plant
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def house_plant_params
+    params.require(:house_plant).permit(:plant_type, :height, :room_id)
+  end
 end
--- app/models/house_plant.rb
+++ app/models/house_plant.rb
@@ -1,3 +1,4 @@
 class HousePlant < ApplicationRecord
   belongs_to :room
+  validates :height, presence: true, numericality: { greater_than_or_equal_to: 1 }
 end
